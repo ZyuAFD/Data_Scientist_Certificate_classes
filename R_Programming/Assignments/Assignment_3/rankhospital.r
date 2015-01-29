b6edc9e7332d1982182f3f1c8e3f410edbe1051c
ZyuAFD <ziwenyu83@gmail.com>
2015-01-29 10:04:09 -0500

    Update rankhospital.r

--- R_Programming/Assignments/Assignment_3/rankhospital.r
+++ R_Programming/Assignments/Assignment_3/rankhospital.r
@@ -1,15 +1,24 @@
-## Function to get the specified rank of hospital based on the lowest 30 day depth rate fo specifed outcome and state
+## Function to get the specified rank of hospital based on the lowest 30 
+## day depth rate fo specifed outcome and state
 rankhospital <- function(state, outcome, num = "best") {
       ## Read outcome data
       Data = read.csv("outcome-of-care-measures.csv", colClasses = "character")
       
       ## Check that state and outcome are valid
       if (!(state %in% unique(Data$State))) stop('invalid state')
-      if (length(grep(gsub(' ','.',outcome),colnames(Data),ignore.case = T))==0) stop('invalid outcome')
+      if (length(grep(gsub(' ','.',outcome)
+                        ,colnames(Data)
+                        ,ignore.case = T)
+                  )==0) stop('invalid outcome')
       
       ## Return hospital name in that state with the given rank
       ## 30-day death rate
-      Data=Data[Data$State==state,c(2,grep(gsub(' ','.',outcome),colnames(Data),ignore.case = T)[1])]
+      Data=Data[Data$State==state
+                  ,c(2,grep(gsub(' ','.',outcome)
+                              ,colnames(Data)
+                              ,ignore.case = T)[1]
+                        )
+                  ]
       Data=Data[which(Data[,2]!='Not Available'),]
       Data[,2]=as.numeric(Data[,2])
       Order = order(Data[,2],Data[,1])      

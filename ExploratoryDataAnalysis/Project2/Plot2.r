02c8d3d6c49ca40497e42087fe4ebdef1baa4422
Ziwen Yu <ziwenyu83@gmail.com>
2015-03-07 14:02:36 -0500

    Create Plot2.r

---
+++ ExploratoryDataAnalysis/Project2/Plot2.r
@@ -0,0 +1,15 @@
+setwd('E:\\Classes\\Data Scientist Certificate\\Exploratory Data Analysis\\Project\\Project 2\\')
+
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+
+library(data.table)
+NEI=data.table(NEI)
+PM25Ann=NEI[NEI$fips=="24510",sum(Emissions),by=year]
+
+barplot(PM25Ann$V1/10^6,
+        names.arg=PM25Ann$year,
+        xlab="Year",
+        ylab=expression(paste('PM2.5 Emissions (',10^6,' Tons)',sep='')),
+        main="Total PM2.5 Emissions From All Baltimore City Sources"
+        )

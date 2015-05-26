533f6c15baadbca8f34dd16c25aedd58e54f28c2
Ziwen Yu <ziwenyu83@gmail.com>
2015-05-26 11:14:24 -0400

    Update ui.R

--- DevelopDataProd/ShinyApp/ui.R
+++ DevelopDataProd/ShinyApp/ui.R
@@ -20,7 +20,16 @@ shinyUI(fluidPage(
                               "Number of Links:",
                               min = 1,
                               max = 100,
-                              value = 20)
+                              value = 20),
+                  h3('Purpose:'),
+                  h4('This App has been developed to understand the effect of fast greedy algorithm
+                     in community detection. Data used here is randomly generated based on the number
+                     of nodes and number of links that user specified.'),
+                  h3('Usage:'),
+                  h4('The user can change the number of nodes and links in the network. A random network 
+                     will then generated. The results is plotted in the main panel with number of links 
+                     sized and communities colored.')
+                  
             ),
             
             # Show a plot of the generated distribution
